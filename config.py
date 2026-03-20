0ec0ad8d58fd35ad1039abef73cdeb1e00428dcd
ivanbaibara <dronegofpv@gmail.com>
2026-03-20 11:13:50 +0300

    Update db directory

--- config.py
+++ config.py
@@ -1,1 +1,5 @@
-DB_FULL_PATH = '/home/ivan/PycharmProjects/metric-service/data/iot_project.db'
+from pathlib import Path
+
+project_root = Path(__file__).resolve().parent
+
+DB_FULL_PATH = f'{project_root}/data/iot_project.db'
